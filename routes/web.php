fba5a08eeb2dffaf281acf0437467567027f61de
programmertocode <children_park@hotmail.com>
2023-03-21 20:26:25 -0600

    laravel proyect modified

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('backend')->group(function() {
+    Route::get('dashboard',[AdminController::class,'dashboard']);
 });
 
 Route::get('/dashboard', function () {
